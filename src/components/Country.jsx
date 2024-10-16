c9bcac89624246a89adeb984bee6aadadbbcd165
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-10-16 12:42:46 -0300

    Add weather info

--- src/components/Country.jsx
+++ src/components/Country.jsx
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 const Country = ({ country }) => {
+  const apiKey = import.meta.env.VITE_OPENWEATHER_KEY;
+
+  const [weather, setWeatherData] = useState(null);
+  const capital = country.capital ? country.capital[0] : "";
+
+  useEffect(() => {
+    if (!capital || !apiKey) return;
+    const baseUrl = `https://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${apiKey}&units=metric`;
+    axios
+      .get(baseUrl)
+      .then((response) => setWeatherData(response.data))
+      .catch((error) => console.error("Error fetching weather data", error));
+  }, [capital, apiKey]);
+
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -10,9 +26,20 @@ const Country = ({ country }) => {
         ))}
       </ul>
       <img
-        src={country.flags.png || country.flag.svg}
+        src={country.flags.png || country.flags.svg}
         alt={country.flags.alt}
       />
+      {weather && (
+        <div>
+          <p>Temperature: {weather.main.temp}°C</p>
+          <p>Wind: {weather.wind.speed} m/s</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={weather.weather[0].description}
+          />
+          <p>{weather.weather[0].description}</p>
+        </div>
+      )}
     </div>
   );
 };
