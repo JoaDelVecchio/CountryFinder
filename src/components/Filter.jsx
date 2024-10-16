06398fb8df796a3e7f16a6d06484bca166a0731b
Joaquin Del Vecchio <172492638+JoaDelVecchio@users.noreply.github.com>
2024-10-16 10:34:01 -0300

    Fix render issue

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,13 +1,14 @@
 const Filter = ({ setFilterValue, filterValue }) => {
-  <div>
-    <p>find countries</p>
-    <input
-      type="text"
-      placeholder="find countries"
-      value={filterValue}
-      onChange={(e) => setFilterValue(e.target.value)}
-    />
-  </div>;
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="find countries"
+        value={filterValue}
+        onChange={(e) => setFilterValue(e.target.value)}
+      />
+    </div>
+  );
 };
 
 export default Filter;
